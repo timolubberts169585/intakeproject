dae525e3d1a9a081a925828a4998f874289c4ed6
koekigamer <171357@student.aventus.nl>
2023-03-20 12:28:25 +0100

    home page

--- home.php
+++ home.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    
-</body>
-</html>
-
 <?php include_once './header.php'; 
 $loggedIn = false;
 if(isset($_SESSION['userid'])){
@@ -23,6 +10,12 @@ if(isset($_SESSION['userid'])){
     <?php 
     ?>
     <!-- Content -->
+    <div class="quizzes">
+        <a class="link" href="">Algemeen</a>
+        <a class="link" href="">HTML</a>
+        <a class="link" href="">CSS</a>
+        <a class="link" href="">Python</a>
+    </div>
 
 </main>
 <?php 
