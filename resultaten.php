a125edaccbde5d661d2883532149e1adaa5a397b
koekigamer <171357@student.aventus.nl>
2023-03-24 14:36:10 +0100

    gif

--- resultaten.php
+++ resultaten.php
@@ -12,6 +12,30 @@ if(isset($_SESSION['userid'])){
     <p>100% goed: Goed gedaan, je hebt al veel kennis voor deze opleiding</p>
     <p>11% 69% goed: Helaas, Je kennis is nog niet goed genoeg. Probeer nog iets meer te leren en overleg met je ouders of deze opleiding echt bij je pas.</p>
     <p>10% of lager goed: je hebt best veel fout, misschien is het handiger om een andere richting te kiezen.</p>
+    <div class="antwoorden">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <ol>
+                    <li>Antwoord</li>
+                    <li>Antwoord</li>
+                    <li>Antwoord</li>
+                    <li>Antwoord</li>
+                    <li>Antwoord</li>
+                    <li>Antwoord</li>
+                    <li>Antwoord</li>
+                    <li>Antwoord</li>
+                    <li>Antwoord</li>
+                    <li>Antwoord</li>
+                </ol>
+            </div>
+            <div class="col-sm-4">
+            <img src="./images/very-good.gif" alt="Your GIF">
+            </div>
+        </div>
+    </div>
+
 
 </main>
 <?php 
