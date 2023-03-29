ea5652c06cecf422f41b8e0e3dbf2f35d4ff6db9
koekigamer <171357@student.aventus.nl>
2023-03-29 15:13:43 +0200

    restultaten

--- resultaten.php
+++ resultaten.php
@@ -9,15 +9,18 @@ if(isset($_SESSION['userid'])){
 <main>
 
     <!-- Content -->
-    <p>100% goed: Goed gedaan, je hebt al veel kennis voor deze opleiding</p>
-    <p>11% 69% goed: Helaas, Je kennis is nog niet goed genoeg. Probeer nog iets meer te leren en overleg met je ouders of deze opleiding echt bij je pas.</p>
-    <p>10% of lager goed: je hebt best veel fout, misschien is het handiger om een andere richting te kiezen.</p>
-    <div class="antwoorden">
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-                <ol>
+    <div class="container pagina">
+        <div id="rij" class="row">
+            <div class="col-sm-12">
+                <h1>resultaten</h1>
+                <p><span class="goed">100%</span> goed: Goed gedaan, je hebt al veel kennis voor deze opleiding</p>
+                <p><span class="matig">11% 69%</span> goed: Helaas, Je kennis is nog niet goed genoeg. Probeer nog iets meer te leren en overleg met je ouders of deze opleiding echt bij je pas.</p>
+                <p><span class="slecht">10%</span> of lager goed: je hebt best veel fout, misschien is het handiger om een andere richting te kiezen.</p>
+            </div>
+        </div>
+        <div id="rij" class="row">
+            <div class="col-sm-12">
+                <ol class="lijst">
                     <li>Antwoord</li>
                     <li>Antwoord</li>
                     <li>Antwoord</li>
@@ -30,8 +33,15 @@ if(isset($_SESSION['userid'])){
                     <li>Antwoord</li>
                 </ol>
             </div>
-            <div class="col-sm-4">
+            <!-- <div class="col-sm-6">
             <img src="./images/very-good.gif" alt="Your GIF">
+            </div> -->
+        </div>
+        <div id="rij" class="row">
+            <div class="col-sm-12">
+                <a href="./home">
+                    <button class="knop">Home</button>
+                </a>
             </div>
         </div>
     </div>
