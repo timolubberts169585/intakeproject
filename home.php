cfcdcb2b8f5438f3850017f09c86843ba7b34acc
koekigamer <171357@student.aventus.nl>
2023-04-03 10:52:16 +0200

    home info

--- home.php
+++ home.php
@@ -19,19 +19,19 @@ if(isset($_SESSION['userid'])){
     <div class="container">
         <div class="row">
             <div class="col-sm-4">
-                <h3>Wat ga je leren</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                <h3>Wat ga je leren?</h3>
+                <p>Er zijn in totaal 4 quizzes. Die bestaan uit Algemeen, HTML, CSS en Python.</p>
+                <p>Je gaat leren programeren. En je krijgt informatie over de opleiding zelf.</p>
             </div>
             <div class="col-sm-4">
-                <h3>Wat voor vragen zijn het</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                <h3>Wat voor quizzes zijn het?</h3>
+                <p>De quizzes bestaan uit 10 vragen waarvan 5 meer keuze vragen en 5 programeer vragen.</p>
+                <p>De Algemene quiz bestaat uit vragen die over de opleiding gaan.</p>
             </div>
             <div class="col-sm-4">
-                <h3>Waarom moet je deze quiz maken</h3>        
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                <h3>Waarom moet je deze quiz maken?</h3>        
+                <p>Je gaat deze quiz maken zodat je er achter komt of deze opleiding bij je past.</p>
+                <p>Ook ga je er achter komen hoeveel kennis je hebt met behulp van deze quiz.</p>
             </div>
         </div>
         <div class="row">
@@ -39,8 +39,7 @@ if(isset($_SESSION['userid'])){
                 <a href="./login.php">
                  <button class="knop">Ouders</button>
                 </a>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                <p>Bent u ouder of een verzorger?<br>log dan in bij Ouders.</p>
             </div>
             <div class="col-sm-4">
                 <img class="foto" src="https://www.aventus.nl/sites/default/files/styles/image/public/images/opleidingen_in_de_it.jpeg?itok=ii5AE-x4&timestamp=1585573307">
@@ -49,8 +48,7 @@ if(isset($_SESSION['userid'])){
                 <a href="./login.php">
                  <button class="knop">Studenten</button>
                 </a>      
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                <p>Ben je student of leerling?<br>log dan in bij Studenten.</p>
             </div>
         </div>
     </div>
